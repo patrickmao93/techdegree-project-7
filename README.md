b4141294bff28014b991322e6e1e2fbfe982e6e3
Patrick <patrickmao93@gmail.com>
2018-12-22 19:58:27 +0800

    added notifications

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
 # A Chart App
+
 This project is deployed with Github Pages, if you don't want to look at the source code, check it out [here](https://patrickmao93.github.io/techdegree-project-7/) <br>
 This was the project #7 of the [Treehouse Front End Web Development Techdegree](https://teamtreehouse.com/techdegree/front-end-web-development-2). <br>
 CSS Grid is just awesomeness. What even better is when combining CSS Grid with flexbox. These two tools really makes laying out web apps a breeze. Grid is incredibly versatile when it comes to 2D layouts, but sometimes I find it to be tedious when a container just contains several elements in one direction, and this is where flexbox comes in handy! In this project, you will see the combined power of CSS Grid and Flexbox, resulting in fully responsive page layouts.<br><br>
-*Please note that this App interface is for front-end practice only. It does not have soul(back-end), so don't expect anything to actually work.*
+
+_Please note that this App interface is for front-end practice only. It does not have soul(back-end), so don't expect anything to actually work._
 
 ## Instruction From Treehouse
-> In this project, you'll take a mockup and a few icons and build a beautiful, web dashboard complete with JavaScript-driven charts and graphs. You only need to take the design and create the HTML, CSS and JavaScript functionality for this one page -- you don't need to create other pages, or build any backend or database functionality.
+
+> In this project, you'll take a mockup and a few icons and build a beautiful, web dashboard complete with JavaScript-driven charts and graphs. You only need to take the design and create the HTML, CSS and JavaScript functionality for this one page -- you don't need to creavte other pages, or build any backend or database functionality.
 
 ## Starter Content From Treehouse
+
 - Mockup image for large screen size (width > 1024px)
 - Icons (in SVG format)
 - Placeholder images
 
 ## Libraries
+
 - Chart.js
 
 ## Deliverable
+
 - A fully responsive front-end of a fictional web dashboard app.
 
 ## Screenshots
+
 <details><summary>Small Screen Size (320px ~ 767px)</summary>
     <img src="screenshots/phone.png" alt="Screenshot Small">
 </details>
--- js/app.js
+++ js/app.js
@@ -1,74 +1,81 @@
 "use strict";
 
 class ToggleButton {
-    constructor(dom, defaultState = 'on') {
-        this.dom = dom;
-        this.state = defaultState;
-        if (defaultState === 'on') {
-            this.dom.children[0].style.left = '0px';
-            this.dom.children[0].style.background = '#727ABF';
-        } else {
-            this.dom.children[0].style.left = '-46px';
-            this.dom.children[0].style.background = '#B3B3B3';
-        }
+  constructor(dom, defaultState = "on") {
+    this.dom = dom;
+    this.state = defaultState;
+    if (defaultState === "on") {
+      this.dom.children[0].style.left = "0px";
+      this.dom.children[0].style.background = "#727ABF";
+    } else {
+      this.dom.children[0].style.left = "-46px";
+      this.dom.children[0].style.background = "#B3B3B3";
     }
+  }
 
-    toggle() {
-        if (this.state === 'on') {
-            this.state = 'off';
-            this.dom.children[0].style.left = '-46px';
-            this.dom.children[0].style.background = '#B3B3B3';
-        } else {
-            this.state = 'on';
-            this.dom.children[0].style.left = '0px';
-            this.dom.children[0].style.background = '#727ABF';
-        }
+  toggle() {
+    if (this.state === "on") {
+      this.state = "off";
+      this.dom.children[0].style.left = "-46px";
+      this.dom.children[0].style.background = "#B3B3B3";
+    } else {
+      this.state = "on";
+      this.dom.children[0].style.left = "0px";
+      this.dom.children[0].style.background = "#727ABF";
     }
+  }
 }
-const toggle1 = document.getElementById('toggle1');
-const toggle2 = document.getElementById('toggle2');
-const checkbox1 = document.getElementById('sendNotification');
-const checkbox2 = document.getElementById('profilePublic');
-const saveBtn = document.getElementById('saveBtn');
+const toggle1 = document.getElementById("toggle1");
+const toggle2 = document.getElementById("toggle2");
+const checkbox1 = document.getElementById("sendNotification");
+const checkbox2 = document.getElementById("profilePublic");
+const timezone = document.getElementById("timezone");
+const saveBtn = document.getElementById("saveBtn");
+const notificationBell = document.getElementById("notificationBell");
+const notifications = document.getElementById("notifications");
 
-const nav = document.getElementById('nav');
-const alert = document.getElementById('message');
+const nav = document.getElementById("nav");
+const alert = document.getElementById("message");
 const search = document.getElementById("search");
-const icnClose = document.getElementById('icnClose');
-const traffic = document.getElementById("chartTraffic").getContext('2d');
-const trafficChartOptions = document.getElementById('trafficChartOptions');
-const dailyTraffic = document.getElementById("chartDailyTraffic").getContext('2d');
-const mobileUsers = document.getElementById("chartMobileUsers").getContext('2d');
-const messageForm = document.getElementById('message-form');
+const icnClose = document.getElementById("icnClose");
+const traffic = document.getElementById("chartTraffic").getContext("2d");
+const trafficChartOptions = document.getElementById("trafficChartOptions");
+const dailyTraffic = document
+  .getElementById("chartDailyTraffic")
+  .getContext("2d");
+const mobileUsers = document
+  .getElementById("chartMobileUsers")
+  .getContext("2d");
+const messageForm = document.getElementById("message-form");
 let searchFocusState = false;
 
 //////////////////////////////////////////////////
 //function showMessage
-//input: name - user name the message is sent to 
+//input: name - user name the message is sent to
 //       type - message type
 //return: none
 //////////////////////////////////////////////////
 const showMessage = (message, type) => {
-    let color;
-    alert.children[0].textContent = message;
-    switch (type) {
-        case 'success':
-            color = '#7DC991';
-            break;
+  let color;
+  alert.children[0].textContent = message;
+  switch (type) {
+    case "success":
+      color = "#7DC991";
+      break;
 
-        case 'warning':
-            color = 'firebrick';
-            break;
+    case "warning":
+      color = "firebrick";
+      break;
 
-        default:
-            alert.children[0].innerHTML = '<span class="bold">Alert: </span>You know nothing about charts, Jon Snow.'
-            color = '#dfc75f';
-            break;
-    }
-    alert.style.borderLeftColor = color;
-    alert.classList.add('message--show');
-}
-
+    default:
+      alert.children[0].innerHTML =
+        '<span class="bold">Alert: </span>You know nothing about charts, Jon Snow.';
+      color = "#dfc75f";
+      break;
+  }
+  alert.style.borderLeftColor = color;
+  alert.classList.add("message--show");
+};
 
 //////////////////////////////////////////////////
 //function hideMessage
@@ -76,9 +83,26 @@ const showMessage = (message, type) => {
 //return: none
 //////////////////////////////////////////////////
 const hideMessage = () => {
-    alert.classList.remove('message--show');
-}
+  alert.classList.remove("message--show");
+};
 
+//////////////////////////////////////////////////
+//function showNotification
+//input: none
+//return: none
+//////////////////////////////////////////////////
+const showNotifications = () => {
+  notifications.style.display = "block";
+};
+
+//////////////////////////////////////////////////
+//function showNotification
+//input: none
+//return: none
+//////////////////////////////////////////////////
+const hideNotifications = () => {
+  notifications.style.display = "none";
+};
 
 //////////////////////////////////////////////////
 //function setChartData
@@ -86,9 +110,9 @@ const hideMessage = () => {
 //return: none
 //////////////////////////////////////////////////
 const setChartData = (chart, labelArr, dataArr, index) => {
-    chart.data.labels = labelArr[index];
-    chart.data.datasets[0].data = dataArr[index];
-}
+  chart.data.labels = labelArr[index];
+  chart.data.datasets[0].data = dataArr[index];
+};
 
 ////////////////////////////////////////////////////////////////////////////////
 //-------------------------------initialization---------------------------------
@@ -97,9 +121,9 @@ const setChartData = (chart, labelArr, dataArr, index) => {
 showMessage();
 
 //if first time here, set both toggle to 'on'
-if (localStorage.sendNotification === undefined || localStorage.profilePublic === undefined) {
-    localStorage.sendNotification = 'on';
-    localStorage.profilePublic = 'off';
+if (!localStorage.sendNotification || !localStorage.profilePublic) {
+  localStorage.sendNotification = "on";
+  localStorage.profilePublic = "off";
 }
 //initialize toggle buttons
 const toggleBtn1 = new ToggleButton(toggle1, localStorage.sendNotification);
@@ -109,221 +133,276 @@ const toggleBtn2 = new ToggleButton(toggle2, localStorage.profilePublic);
 //-------------------------UI nteraction handlers---------------------------------
 //////////////////////////////////////////////////////////////////////////////////
 for (let i = 0; i < nav.children.length; i++) {
-    let navItem = nav.children[i];
-    navItem.addEventListener('click', e => {
-        let targetDiv = document.getElementById('widget' + (i + 1));
-        targetDiv.scrollIntoView({
-            behavior: 'smooth', 
-            block: 'start'
-        });
-        
-        for (let j = 0; j < nav.children.length; j++) {
-            let navItem = nav.children[j];
-            navItem.classList.remove('sidebar__icon--active');
-        }
-
-        navItem.classList.add('sidebar__icon--active');
+  let navItem = nav.children[i];
+  navItem.addEventListener("click", e => {
+    let targetDiv = document.getElementById("widget" + (i + 1));
+    targetDiv.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
     });
+
+    for (let j = 0; j < nav.children.length; j++) {
+      let navItem = nav.children[j];
+      navItem.classList.remove("sidebar__icon--active");
+    }
+
+    navItem.classList.add("sidebar__icon--active");
+  });
 }
 
+notificationBell.addEventListener("click", e => {
+  e.stopPropagation();
+  showNotifications();
+});
+
+notifications.addEventListener("click", e => {
+  e.stopPropagation();
+});
+
+window.addEventListener("click", e => {
+  hideNotifications();
+});
+
 //close message when user clicks on x
-icnClose.addEventListener('click', e => {
-    hideMessage();
+icnClose.addEventListener("click", e => {
+  hideMessage();
 });
 
 //search hover state and edit state functionsalities
-search.addEventListener('mouseenter', e => {
-    search.classList.add('search__input--is-hovered');
+search.addEventListener("mouseenter", e => {
+  search.classList.add("search__input--is-hovered");
 });
-search.addEventListener('mouseleave', e => {
-    if (!searchFocusState) {
-        search.classList.remove('search__input--is-hovered');
-    }
+search.addEventListener("mouseleave", e => {
+  if (!searchFocusState) {
+    search.classList.remove("search__input--is-hovered");
+  }
 });
-search.addEventListener('focusin', e => {
-    searchFocusState = true;
-    search.classList.add('search__input--is-focused');
+search.addEventListener("focusin", e => {
+  searchFocusState = true;
+  search.classList.add("search__input--is-focused");
 });
-search.addEventListener('focusout', e => {
-    searchFocusState = false;
-    search.classList.remove('search__input--is-hovered');
-    search.classList.remove('search__input--is-focused');
+search.addEventListener("focusout", e => {
+  searchFocusState = false;
+  search.classList.remove("search__input--is-hovered");
+  search.classList.remove("search__input--is-focused");
 });
+
+timezone.addEventListener("click", e => {});
 
 //On form submit, inform user message is sent
-messageForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const name = document.getElementById('sendName').value;
-    const message = document.getElementById('sendMessage').value;
-    console.log(message);
-    
-    let alertMsg;
+messageForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const name = document.getElementById("sendName").value;
+  const message = document.getElementById("sendMessage").value;
+  console.log(message);
 
-    if (name && message) {
-        alertMsg = 'Message successfully sent to ' + name;
-        showMessage(alertMsg, 'success'); 
-    } else {
-        alertMsg = 'Please check your input'
-        showMessage(alertMsg, 'warning'); 
-    }  
-    //hide message after 3 secs
-    setTimeout(() => {
-        hideMessage();
-    }, 3000);
+  let alertMsg;
+
+  if (name && message) {
+    alertMsg = "Message successfully sent to " + name;
+    showMessage(alertMsg, "success");
+  } else {
+    alertMsg = "Please check your input";
+    showMessage(alertMsg, "warning");
+  }
+  //hide message after 3 secs
+  setTimeout(() => {
+    hideMessage();
+  }, 3000);
 });
 
 //toggle button 1 functionality
-toggle1.addEventListener('click', e => {
-    toggleBtn1.toggle();
+toggle1.addEventListener("click", e => {
+  toggleBtn1.toggle();
 });
 //toggle button 2 functionality
-toggle2.addEventListener('click', e => {
-    toggleBtn2.toggle();
+toggle2.addEventListener("click", e => {
+  toggleBtn2.toggle();
 });
 
-saveBtn.addEventListener('click', e => {
-    localStorage.sendNotification = toggleBtn1.state;
-    localStorage.profilePublic = toggleBtn2.state;
+saveBtn.addEventListener("click", e => {
+  localStorage.sendNotification = toggleBtn1.state;
+  localStorage.profilePublic = toggleBtn2.state;
 });
-
 
 //////////////////////////////////////////////////////////////////////////////////
 //-----------------------------Chart initialization-------------------------------
 //////////////////////////////////////////////////////////////////////////////////
 //--------------------------------traffic chart---------------------------------
 const trafficLabels = [
-    ["0:00", "2:00", "4:00", "6:00", "8:00", "10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00"],
-    ["day1", "day2", "day3", "day4", "day5", "day6", "day7"],
-    ["week1", "week2", "week3", "week3", "week4", "week5", "week6", "week7", "week8", "week9", "week10", "week11"],
-    ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
+  [
+    "0:00",
+    "2:00",
+    "4:00",
+    "6:00",
+    "8:00",
+    "10:00",
+    "12:00",
+    "14:00",
+    "16:00",
+    "18:00",
+    "20:00",
+    "22:00"
+  ],
+  ["day1", "day2", "day3", "day4", "day5", "day6", "day7"],
+  [
+    "week1",
+    "week2",
+    "week3",
+    "week3",
+    "week4",
+    "week5",
+    "week6",
+    "week7",
+    "week8",
+    "week9",
+    "week10",
+    "week11"
+  ],
+  [
+    "JAN",
+    "FEB",
+    "MAR",
+    "APR",
+    "MAY",
+    "JUN",
+    "JUL",
+    "AUG",
+    "SEP",
+    "OCT",
+    "NOV",
+    "DEC"
+  ]
 ];
 const trafficData = [
-    [750, 1250, 750, 750, 1750, 1750, 1250, 1000, 800, 930, 300, 400],
-    [1000, 1200, 900, 935, 950, 1250, 1400],
-    [0, 750, 1250, 1000, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2250],
-    [800, 900, 800, 834, 855, 890, 1000, 988, 912, 940, 1032, 1100],
+  [750, 1250, 750, 750, 1750, 1750, 1250, 1000, 800, 930, 300, 400],
+  [1000, 1200, 900, 935, 950, 1250, 1400],
+  [0, 750, 1250, 1000, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2250],
+  [800, 900, 800, 834, 855, 890, 1000, 988, 912, 940, 1032, 1100]
 ];
 
 const chartTraffic = new Chart(traffic, {
-    // The type of chart we want to create
-    type: 'line',
+  // The type of chart we want to create
+  type: "line",
 
-    // The data for our dataset
-    data: {
-        labels: trafficLabels[2],
-        datasets: [{
-            backgroundColor: 'rgb(116, 122, 191, 0.3)',
-            borderColor: 'rgb(116, 122, 191)',
-            borderWidth: '1',
-            pointRadius: '6',
-            pointBackgroundColor: '#FFFFFF',
-            pointBorderWidth: '2',
-            data: trafficData[2],
-            lineTension: 0
-        }]
-    },
-    options: {
-        responsive: true,
-        aspectiRatio: 3,
-        legend: false,
-    }
+  // The data for our dataset
+  data: {
+    labels: trafficLabels[2],
+    datasets: [
+      {
+        backgroundColor: "rgb(116, 122, 191, 0.3)",
+        borderColor: "rgb(116, 122, 191)",
+        borderWidth: "1",
+        pointRadius: "6",
+        pointBackgroundColor: "#FFFFFF",
+        pointBorderWidth: "2",
+        data: trafficData[2],
+        lineTension: 0
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    aspectiRatio: 3,
+    legend: false
+  }
 });
 
 //traffic chart "display by time" options
-trafficChartOptions.addEventListener('click', e => {
-    let target = e.target;
-    //add/remove highlight
-    if (target.className !== 'chart-header__options') { //prevents from clicking on ul 
-        target.classList.add('chart-header__options__item--active');
-        let lis = trafficChartOptions.children;
-        for (let i = 0; i < lis.length; i++) {
-            let li = lis[i];
-            if (li !== target) {
-                li.classList.remove('chart-header__options__item--active');
-            }
-        }
+trafficChartOptions.addEventListener("click", e => {
+  let target = e.target;
+  //add/remove highlight
+  if (target.className !== "chart-header__options") {
+    //prevents from clicking on ul
+    target.classList.add("chart-header__options__item--active");
+    let lis = trafficChartOptions.children;
+    for (let i = 0; i < lis.length; i++) {
+      let li = lis[i];
+      if (li !== target) {
+        li.classList.remove("chart-header__options__item--active");
+      }
     }
+  }
 
-    switch (target.textContent) {
-        case 'Hourly':
-            setChartData(chartTraffic, trafficLabels, trafficData, 0);
-            break;
+  switch (target.textContent) {
+    case "Hourly":
+      setChartData(chartTraffic, trafficLabels, trafficData, 0);
+      break;
 
-        case 'Daily':
-            setChartData(chartTraffic, trafficLabels, trafficData, 1);
-            break;
+    case "Daily":
+      setChartData(chartTraffic, trafficLabels, trafficData, 1);
+      break;
 
-        case 'Weekly':
-            setChartData(chartTraffic, trafficLabels, trafficData, 2);
-            break;
+    case "Weekly":
+      setChartData(chartTraffic, trafficLabels, trafficData, 2);
+      break;
 
-        case 'Monthly':
-            setChartData(chartTraffic, trafficLabels, trafficData, 3);
-            break;
+    case "Monthly":
+      setChartData(chartTraffic, trafficLabels, trafficData, 3);
+      break;
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 
-    chartTraffic.update();
+  chartTraffic.update();
 });
 
 //--------------------------------end traffic chart---------------------------------
 
-
 //--------------------------------daily traffic chart---------------------------------
 const chartDailyTraffic = new Chart(dailyTraffic, {
-    // The type of chart we want to create
-    type: 'bar',
+  // The type of chart we want to create
+  type: "bar",
 
-    // The data for our dataset
-    data: {
-        labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-        datasets: [{
-            backgroundColor: 'rgb(116, 122, 191)',
-            data: [75, 100, 175, 125, 225, 200, 100]
-        }]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: true,
-        legend: false,
-    }
+  // The data for our dataset
+  data: {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [
+      {
+        backgroundColor: "rgb(116, 122, 191)",
+        data: [75, 100, 175, 125, 225, 200, 100]
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: true,
+    legend: false
+  }
 });
 //--------------------------------end daily traffic chart---------------------------------
 
 //--------------------------------Mobile users chart---------------------------------
 const mobileUsersChart = new Chart(mobileUsers, {
-    type: 'doughnut',
-    data: {
-        datasets: [{
-            data: [10, 10, 40],
-            backgroundColor: ['#7DC991', '#6FB2C0', '#727ABF'],
-        }],
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: [
-            'Phones',
-            'Tablets',
-            'Desktop'
-        ]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: true,
-        legend: {
-            position: 'right',
-            labels: {
-                boxWidth: 20,
-                fontSize: 14,
-                padding: 22,
-            }
-        }
+  type: "doughnut",
+  data: {
+    datasets: [
+      {
+        data: [10, 10, 40],
+        backgroundColor: ["#7DC991", "#6FB2C0", "#727ABF"]
+      }
+    ],
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: ["Phones", "Tablets", "Desktop"]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: true,
+    legend: {
+      position: "right",
+      labels: {
+        boxWidth: 20,
+        fontSize: 14,
+        padding: 22
+      }
     }
+  }
 });
 //--------------------------------end mobile users chart---------------------------------
 
-//When 'responsive' option is set to 'true', the charts are supposed to resize themselves upon window resize. 
+//When 'responsive' option is set to 'true', the charts are supposed to resize themselves upon window resize.
 //This part exists because there is a weird bug prevents the charts to resize properly when window is sized down
-window.addEventListener('resize', e => {
-    chartDailyTraffic.resize();
+window.addEventListener("resize", e => {
+  chartDailyTraffic.resize();
 });
